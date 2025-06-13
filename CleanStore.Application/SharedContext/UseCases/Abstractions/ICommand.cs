7380ec56626ec3c49cb418b3a6ed8274ebc5fc23
sorza <alexandre.sorza@outlook.com>
2025-06-12 22:49:51 -0300

    Definindo ICommandHandler

--- CleanStore.Application/SharedContext/UseCases/Abstractions/ICommand.cs
+++ CleanStore.Application/SharedContext/UseCases/Abstractions/ICommand.cs
@@ -4,4 +4,7 @@ using MediatR;
 namespace CleanStore.Application.SharedContext.UseCases.Abstractions
 {
     public interface ICommand : IRequest<Result>;
+
+    public interface ICommand<TCommandResponse> : IRequest<Result<TCommandResponse>>
+        where TCommandResponse : ICommandResponse;
 }
---
+++ CleanStore.Application/SharedContext/UseCases/Abstractions/ICommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using CleanStore.Application.SharedContext.Results;
+using MediatR;
+
+namespace CleanStore.Application.SharedContext.UseCases.Abstractions
+{
+    public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand, Result> 
+        where TCommand : ICommand
+    {
+    }
+
+    public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+        where TCommand : ICommand<TResponse>
+        where TResponse : ICommandResponse
+    {
+        
+    }
+}
