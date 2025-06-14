fd043e69fbec6a8bb5858421b8dd2fb745b99c33
sorza <alexandre.sorza@outlook.com>
2025-06-14 13:42:52 -0300

    Criando AccountRepository e registrando a dependencia

---
+++ CleanStore.Infrastructure/AccountContext/Repositories/AccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using CleanStore.Application.AccountContext.Repositories.Abstractions;
+using CleanStore.Domain.AccountContext.Entities;
+using CleanStore.Infrastructure.SharedContext.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanStore.Infrastructure.AccountContext.Repositories
+{
+    public class AccountRepository(AppDbContext context) : IAccountRepository
+    {
+        public async Task SaveAsync(Account account) 
+            => await context.Accounts.AddAsync(account);
+
+        public async Task<bool> VerifyEmailExistsAsync(string email) 
+            => await context.Accounts.AsNoTracking().AnyAsync(a => a.Email == email);
+        
+    }
+}
--- CleanStore.Infrastructure/SharedContext/DependencyInjection.cs
+++ CleanStore.Infrastructure/SharedContext/DependencyInjection.cs
@@ -1,7 +1,19 @@
-﻿namespace CleanStore.Infrastructure.SharedContext
-{
-    public class DependencyInjection
-    {
+﻿using CleanStore.Application.AccountContext.Repositories.Abstractions;
+using CleanStore.Application.SharedContext.Repositories.Abstractions;
+using CleanStore.Infrastructure.AccountContext.Repositories;
+using CleanStore.Infrastructure.SharedContext.Data;
+using Microsoft.Extensions.DependencyInjection;
 
+namespace CleanStore.Infrastructure.SharedContext
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<IUnityOfWork, UnitOfWork>();
+            services.AddTransient<IAccountRepository, AccountRepository>();
+
+            return services;
+        }
     }
 }
