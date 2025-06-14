a508648a56d99690eefe8715d729d384380484ff
sorza <alexandre.sorza@outlook.com>
2025-06-14 13:35:46 -0300

    Implementando Infra/UnitOfWork

---
+++ CleanStore.Infrastructure/SharedContext/Data/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using CleanStore.Application.SharedContext.Repositories.Abstractions;
+
+namespace CleanStore.Infrastructure.SharedContext.Data
+{
+    public class UnitOfWork(AppDbContext context) : IUnityOfWork
+    {
+        public async Task CommitAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+
+        public  Task RollbackAsync() => Task.CompletedTask;
+    }
+}
